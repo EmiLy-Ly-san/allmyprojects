817e59974936f3518ac500b5df7461318bb65986
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-10-02 18:03:09 +0200

    script.js added

---
+++ script.js
@@ -0,0 +1,16 @@
+const sortAllButton = document.querySelector(".sortAllButton");
+
+const sortHtmlButton = document.querySelector(".sortHtmlButton");
+const sortCssButton = document.querySelector(".sortCssButton");
+const sortJsButton = document.querySelector(".sortJsButton");
+const sortBstpButton = document.querySelector(".sortJsButton");
+
+const pinnedProjects = document.getElementsByClassName(".pinnedProjects");
+
+let htmlProjects = document.querySelectorAll(".htmlProject");
+let cssProjects = document.querySelectorAll(".cssProject");
+let jsProjects = document.querySelectorAll(".jsProject");
+let allProjects = document.querySelectorAll(".project");
+
+console.log(sortHtmlButton);
+console.log(pinnedProjects);
