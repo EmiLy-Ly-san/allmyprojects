b1c4cf14e1ff53676cf02a7acf748730449d678d
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-10-03 19:42:59 +0200

    animation display grid

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const sortAllButton = document.querySelector(".sortAllButton");
-
 const sortHtmlButton = document.querySelector(".sortHtmlButton");
-const myButton = document.querySelector(".button");
 const sortCssButton = document.querySelector(".sortCssButton");
 const sortJsButton = document.querySelector(".sortJsButton");
 const sortBstpButton = document.querySelector(".sortBstpButton");
+
 const allSentences = document.querySelectorAll(".sentence");
 const pinnedProjectsList = document.querySelector("#projectsGridContainer");
 const allProjects = document.querySelectorAll(".project");
-const allProjectsAndSentences = document.querySelectorAll("article");
+const htmlProjects = document.querySelectorAll(".htmlProject");
+const cssProjects = document.querySelectorAll(".cssProject");
+const jsProjects = document.querySelectorAll(".jsProject");
+const bstProjects = document.querySelectorAll(".bootstrapProject");
 
-let htmlProjects = document.querySelectorAll(".htmlProject");
-let cssProjects = document.querySelectorAll(".cssProject");
-let jsProjects = document.querySelectorAll(".jsProject");
-let bstProjects = document.querySelectorAll(".bootstrapProject");
+Array.from(allProjects).forEach((project, index) => {
+  project.style.animationDelay = `${index * 150}ms`;
+  project.addEventListener("animationend", () => {
+    project.classList.add("visible");
+  });
+});
 
-/*SORT BY HTML PROJECTS*/
-sortHtmlButton.addEventListener("click", () => {
+function reorganizeGrid(typeOfProjectsSelector) {
   allProjects.forEach((project) => {
+    project.classList.remove("visible");
     project.classList.add("display-none");
   });
   allSentences.forEach((sentence) => {
@@ -25,12 +29,17 @@ sortHtmlButton.addEventListener("click", () => {
   });
   pinnedProjectsList.classList.remove("pinnedProjects");
   pinnedProjectsList.classList.add("sortedProjects");
-  const newsortedProjectArray = Array.from(htmlProjects);
-  newsortedProjectArray.forEach((project) => {
-    setTimeout(function () {
+  const newsortedProjectArray = Array.from(typeOfProjectsSelector);
+  setTimeout(function () {
+    newsortedProjectArray.forEach((project) => {
       project.classList.remove("display-none");
-    }, 20);
-  });
+    });
+  }, 100);
+}
+
+/*SORT BY HTML PROJECTS*/
+sortHtmlButton.addEventListener("click", () => {
+  reorganizeGrid(htmlProjects);
   sortHtmlButton.classList.add("active");
   sortAllButton.classList.remove("active");
   sortCssButton.classList.remove("active");
@@ -38,39 +47,9 @@ sortHtmlButton.addEventListener("click", () => {
   sortBstpButton.classList.remove("active");
 });
 
-/*SORT BY ALL PROJECTS*/
-sortAllButton.addEventListener("click", () => {
-  allSentences.forEach((sentence) => {
-    sentence.classList.remove("display-none");
-  });
-  allProjects.forEach((project) => {
-    project.classList.remove("display-none");
-  });
-  pinnedProjectsList.classList.add("pinnedProjects");
-  pinnedProjectsList.classList.remove("sortedProjects");
-  sortHtmlButton.classList.remove("active");
-  sortAllButton.classList.add("active");
-  sortCssButton.classList.remove("active");
-  sortJsButton.classList.remove("active");
-  sortBstpButton.classList.remove("active");
-});
-
 /*SORT BY CSS PROJECTS*/
 sortCssButton.addEventListener("click", () => {
-  allSentences.forEach((sentence) => {
-    sentence.classList.add("display-none");
-  });
-  allProjects.forEach((project) => {
-    project.classList.add("display-none");
-  });
-  pinnedProjectsList.classList.remove("pinnedProjects");
-  pinnedProjectsList.classList.add("sortedProjects");
-  const newsortedProjectArray = Array.from(cssProjects);
-  newsortedProjectArray.forEach((project) => {
-    setTimeout(function () {
-      project.classList.remove("display-none");
-    }, 20);
-  });
+  reorganizeGrid(cssProjects);
   console.log({ cssSortArray: newsortedProjectArray });
   sortHtmlButton.classList.remove("active");
   sortAllButton.classList.remove("active");
@@ -81,20 +60,7 @@ sortCssButton.addEventListener("click", () => {
 
 /*SORT BY JS PROJECTS*/
 sortJsButton.addEventListener("click", () => {
-  allSentences.forEach((sentence) => {
-    sentence.classList.add("display-none");
-  });
-  allProjects.forEach((project) => {
-    project.classList.add("display-none");
-  });
-  pinnedProjectsList.classList.remove("pinnedProjects");
-  pinnedProjectsList.classList.add("sortedProjects");
-  const newsortedProjectArray = Array.from(jsProjects);
-  newsortedProjectArray.forEach((project) => {
-    setTimeout(function () {
-      project.classList.remove("display-none");
-    }, 20);
-  });
+  reorganizeGrid(jsProjects);
   sortHtmlButton.classList.remove("active");
   sortAllButton.classList.remove("active");
   sortCssButton.classList.remove("active");
@@ -104,23 +70,28 @@ sortJsButton.addEventListener("click", () => {
 
 /*SORT BY BOOTSTRAP PROJECTS*/
 sortBstpButton.addEventListener("click", () => {
-  allSentences.forEach((sentence) => {
-    sentence.classList.add("display-none");
-  });
-  allProjects.forEach((project) => {
-    project.classList.add("display-none");
-  });
-  pinnedProjectsList.classList.remove("pinnedProjects");
-  pinnedProjectsList.classList.add("sortedProjects");
-  const newsortedProjectArray = Array.from(bstProjects);
-  newsortedProjectArray.forEach((project) => {
-    setTimeout(function () {
-      project.classList.remove("display-none");
-    }, 20);
-  });
+  reorganizeGrid(bstProjects);
   sortHtmlButton.classList.remove("active");
   sortAllButton.classList.remove("active");
   sortCssButton.classList.remove("active");
   sortJsButton.classList.remove("active");
   sortBstpButton.classList.add("active");
+});
+
+/*SORT BY ALL PROJECTS*/
+sortAllButton.addEventListener("click", () => {
+  allSentences.forEach((sentence) => {
+    sentence.classList.remove("display-none");
+  });
+  allProjects.forEach((project) => {
+    project.classList.remove("visible");
+    project.classList.remove("display-none");
+  });
+  pinnedProjectsList.classList.add("pinnedProjects");
+  pinnedProjectsList.classList.remove("sortedProjects");
+  sortHtmlButton.classList.remove("active");
+  sortAllButton.classList.add("active");
+  sortCssButton.classList.remove("active");
+  sortJsButton.classList.remove("active");
+  sortBstpButton.classList.remove("active");
 });
